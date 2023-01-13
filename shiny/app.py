94beb027813c25189d353b8c94a18f31bebbfec6
Jeff Newmiller <jdnewmil@gmail.com>
2023-01-13 11:51:41 -0800

    add calc columns and info tables

--- shiny/app.py
+++ shiny/app.py
@@ -1,16 +1,33 @@
 # test.py
 
 #import datetime
+import pathlib
 import pandas as pd
 import data_review1
 import data_sel
 import data_any1
+import solar_modeling_calcs as smc
 from shiny import App, ui, reactive, render
 
+
+prjdir = pathlib.Path(__file__).parent.parent
+dtadir = prjdir / 'data'
+cfgdir = dtadir / 'config'
+cachedir = dtadir / 'cache'
+
 # global data for review
-dta = pd.read_parquet('data/cache/dta.parquet')
+dta = pd.read_parquet(cachedir / 'dta.parquet')
 dta.index.name = 'timestamp'
-dta_info = pd.read_csv('data/config/best_dta_col_info.csv')
+dta_info = pd.read_csv(cfgdir / 'best_dta_col_info.csv')
+site_info = pd.read_csv(
+    cfgdir / 'site_info.csv'
+    , index_col=0
+    ).loc['BEST', :]
+sys_info = pd.read_csv(
+    cfgdir / 'sys_info.csv'
+    , index_col=[0, 1])
+dta = smc.add_solar_geometry(dta, site_info=site_info)
+dta = smc.add_array_irradiance(dta, sys_info=sys_info)
 
 
 def create_ui(dta: pd.DataFrame, dta_info: pd.DataFrame):
@@ -18,13 +35,17 @@ def create_ui(dta: pd.DataFrame, dta_info: pd.DataFrame):
 
     Parameters
     ----------
-    dta : list
-        _description_
+    dta : pd.DataFrame
+        Datetime-indexed data frame of time-series data
+    dta_info : pd.DataFrame
+        Dataframe of descriptive categories for columns in
+        `dta`.
 
     Returns
     -------
-    _type_
-        _description_
+    list
+        List of HTML text strings defining the web page that will
+        be managed with the server.
     """
     # create our ui object
     app_ui = ui.page_fluid(
@@ -39,7 +60,14 @@ def create_ui(dta: pd.DataFrame, dta_info: pd.DataFrame):
                         , data_review1.create_ui(id='first'))
                     , ui.nav(
                         'Data Availability'
-                        , data_any1.create_ui(id='data_any_plot'))))))
+                        , data_any1.create_ui(id='data_any_plot'))
+                    , ui.nav(
+                        'Site Info'
+                        , ui.output_table(id='rendered_site_info'))
+                    , ui.nav(
+                        'System Info'
+                        , ui.output_table(id='rendered_sys_info'))
+                    , ))))
     return app_ui
 
 
@@ -55,6 +83,11 @@ def create_server(
     ----------
     dta : pd.DataFrame
         Dataframe to review in server
+
+    Returns
+    -------
+    callable
+        Function to respond to web page activity.
     """
     def server(input, output, session):
         dtar = reactive.Value(dta)
@@ -63,6 +96,18 @@ def create_server(
 
         @reactive.Calc
         def dtalr():
+            """Long-shaped reactive data function.
+
+            Returns
+            -------
+            pd.DataFrame
+                Contains columns:
+                timestamp: datetime[ns]
+                variable: str
+                    Name of parameter
+                value: float
+                    Value of parameter
+            """
             dta_sel1 = dta_selr()
             return (
                 dta_sel1
@@ -86,6 +131,17 @@ def create_server(
         data_any1.server(
             'data_any_plot'
             , dta_selr=dta_selr)
+
+        @output()
+        @render.table(index=True)
+        def rendered_site_info():
+            return pd.DataFrame({
+                'BEST' : site_info}).T
+
+        @output()
+        @render.table(index=True)
+        def rendered_sys_info():
+            return sys_info
 
     return server
 
