b522b2e43261b7829032b5378e809c8d19e52c7c
Jeff Newmiller <jeff.newmiller@dnv.com>
2023-01-17 14:58:23 -0800

    Update readme and app prep and dependencies

--- Readme.md
+++ Readme.md
@@ -6,7 +6,7 @@ data collected from the National Renewable Energy Laboratory Bifacial Experiment
 
 ## Prep for using the app
 
-Prepare a virtual Python environment based on the supplied `requirements.txt`. This app was developed using a `conda` virtual environment, but a `venv` could possibly be used instead.
+Prepare a virtual Python environment based on the supplied `requirements.txt`. This app was developed using a `conda` virtual environment named `best`, but a `venv` could possibly be used instead.
 
 ```bash
 conda env create -f environment.yml
@@ -14,18 +14,32 @@ conda env create -f environment.yml
 # pip install -r requirements.txt 
 ```
 
-## Download data to review
-
-The `prep1.py` script downloads four csv files and combines them into one `data/cache/dta.parquet` file.
+The `src` directory contains the shiny app and supporting python code as a package that needs to be linked into the virtual environment. 
 
 ```bash
+conda activate best
+pip install --no-deps -e src
+```
+
+## Download data to review
+
+The `data` directory in the top-level project directory contains the core configuration files that will be used by the app.
+
+The `src/best_data_review/prep1.py` script downloads four csv files into `data/raw` as csv files and combines them into one `data/cache/dta.parquet` file. It can be invoked from the commandline once the package is installed:
+
+```bash
+best-prep1  # invokes the prep1.py script for you
 # from the top level directory of this repository
-python src/prep1.py  #
+# python src/best_data_review/prep1.py  #
 ```
 
 ## Invoke the app
 
 ```bash
+best-app
 # from the top level directory of this repository
-shiny run --host localhost --port 5000 --launch-browser shiny/app.py
+# shiny run --host localhost --port 5000 --launch-browser shiny/app.py
 ```
+
+The app will open a web page to support user interactions, and remain active to support actions triggered through that web page. Ctrl-C can be used in the command window to stop serving the app. Once the server has stopped, the browser tab will become disabled, and the user must close it.
+
--- src/best_data_review/prep1.py
+++ src/best_data_review/prep1.py
@@ -1,15 +1,9 @@
 # prep1.py
 
-# %%
 import urllib.request as ur
-from joblib import Memory
 import pandas as pd
 import pathlib
 
-# %%
-cachedir = 'data/cache'
-rawdir = 'data/raw'
-mem = Memory(cachedir)
 best_urls = {
     '2020-jan-dec': r'https://datahub.duramat.org/dataset/72334fe1-fa52-45f8-a43c-836a35b9c526/resource/b7e35897-4d32-48fc-b53c-7b780537d87d/download/data_release__sep2022_period_2020_0112.csv'
     , '2021-jan-dec': r'https://datahub.duramat.org/dataset/72334fe1-fa52-45f8-a43c-836a35b9c526/resource/264212b8-800e-47b5-80ac-1dd2c644aa74/download/data_release__sep2022_period_2021_0112.csv'
@@ -17,23 +11,25 @@ best_urls = {
     , '2019-jul-dec': r'https://datahub.duramat.org/dataset/72334fe1-fa52-45f8-a43c-836a35b9c526/resource/f32ba09d-f750-4746-82ec-5f930c18437a/download/data_release_sep2022_period_2019_0712.csv'
 }
 
-# %%
-dtadir = pathlib.Path('data')
-if not dtadir.exists():
-    raise Exception('data directory does exist: are you in the right working directory?')
-elif not dtadir.is_dir():
-    raise Exception('data name in filesystem is not a directory: rename or move existing file, or move to correct working directory')
-elif not (dtadir / 'config').is_dir():
-    raise Exception('data directory does not have a config subdirectory: are you in the right working directory?')
-cachedir = dtadir / 'cache'
-if not cachedir.exists():
-    cachedir.mkdir()
-rawdir = dtadir / 'raw'
-if not rawdir.exists():
-    rawdir.mkdir()
 
-# %%
-def url_fname(best_url_key: str) -> pathlib.Path:
+def prep_dirs() -> tuple[pathlib.Path, pathlib.Path, pathlib.Path]:
+    """Set up working directories."""
+    dtadir = pathlib.Path('data')
+    if not dtadir.exists():
+        raise Exception('data directory does exist: are you in the right working directory?')
+    elif not dtadir.is_dir():
+        raise Exception('"data" name in filesystem is not a directory: rename or move existing file, or move to correct working directory')
+    elif not (dtadir / 'config').is_dir():
+        raise Exception('data directory does not have a config subdirectory: are you in the right working directory?')
+    cachedir = dtadir / 'cache'
+    cachedir.mkdir(exist_ok=True)
+    rawdir = dtadir / 'raw'
+    rawdir.mkdir(exist_ok=True)
+    return dtadir, cachedir, rawdir
+
+
+def url_fname(best_url_key: str, rawdir: pathlib.Path) -> pathlib.Path:
+    """Make local filename for storing BEST data."""
     return rawdir / f'{best_url_key}.csv'
 
 # Define a function to safely read the data
@@ -42,8 +38,21 @@ def url_fname(best_url_key: str) -> pathlib.Path:
 def read_best_data(
     best_url_key: str
     , best_url: str
-) -> pd.DataFrame:
-    fname = url_fname(best_url_key)
+    , rawdir: pathlib.Path
+):
+    """Download data from Duramat Datahub.
+
+    Parameters
+    ----------
+    best_url_key : str
+        Key of url to download. Used in local filename.
+    best_url : str
+        URL of source data.
+    rawdir : pathlib.Path
+        Directory for storing raw data files.
+
+    """
+    fname = url_fname(best_url_key, rawdir=rawdir)
     if not fname.exists():
         print(f'Downloading {fname}')
         ur.urlretrieve(
@@ -53,26 +62,28 @@ def read_best_data(
         print(f'{fname} already exists')
 
 
-# %%
-# download files to raw data directory
-print('Downloading...')
-for k, u in best_urls.items():
-    read_best_data(k, u)
+def prep1_data():
+    """Prepare data directories for analysis."""
+    # prep directories
+    dtadir, cachedir, rawdir = prep_dirs()
+    # download files to raw data directory
+    print('Downloading...')
+    for k, u in best_urls.items():
+        read_best_data(k, u, rawdir=rawdir)
+    # assemble all data into one dataframe
+    print('Combining raw files...')
+    dta = pd.concat([
+        pd.read_csv(
+            url_fname(k, rawdir=rawdir)
+            , parse_dates=True
+            , index_col=0)
+        for k in best_urls.keys()])
+    # insure sorted
+    dta.sort_index(inplace=True)
+    # Save parquet of all data into cache
+    print('Saving combined dataframes to cache...')
+    dta.to_parquet(cachedir / 'dta.parquet')
 
 
-# %%
-# assemble all data into one dataframe
-print('Combining raw files...')
-dta = pd.concat([
-    pd.read_csv(
-        url_fname(k)
-        , parse_dates=True
-        , index_col=0)
-    for k in best_urls.keys()])
-
-# %%
-# Save parquet of all data into cache
-print('Saving combined dataframes to cache...')
-dta.to_parquet(cachedir / 'dta.parquet')
-
-# %%
+if __name__ == "__main__":
+    prep1_data()
